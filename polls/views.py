4adb4fc7be67049357bf8cdc693eeeb64d910c22
MackRoe <elaine.music@students.makeschool.com>
2019-11-22 16:10:17 -0800

    add tests. all tests pass but 1

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-# from django.template import loader
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -17,14 +17,24 @@ class IndexView(generic.ListView):
     # return render(request, 'polls/index.html', context)
 
     def get_queryset(self):
-        """ return last five published questions """
-        return Question.objects.order_by('-pub_date')[:5]
+        """ return last five published questions
+        (not including those set to be published in the future)
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        # -- previous code - commented out --
+        # Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     # question = get_object_or_404(Question, pk=question_id)
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        '''
+        Excludes any questions that aren't published yet.
+        '''
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):

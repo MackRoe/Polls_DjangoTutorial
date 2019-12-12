cd87f79a29de33075a98e27ecf72e1c4ee5d48f5
MackRoe <elaine.music@students.makeschool.com>
2019-12-12 07:17:20 -0800

    modify admin, finish Tutorial

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,9 @@ class Question(models.Model):
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
         # -- previous code, commented out --
         # self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
